5d47baf4781846b45006992c69eb56e1c0460c60
BuildTools <unconfigured@null.spigotmc.org>
2020-10-31 12:13:24 -0700

    10/31 Joshua. Localization Testing and more.

--- TeamCode/src/main/java/org/wolfcorp/ultimategoal/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/wolfcorp/ultimategoal/drive/DriveConstants.java
@@ -55,7 +55,7 @@ public class DriveConstants {
      */
     public static double kV = 0.01115;
     public static double kA = 5;
-    public static double kStatic = 0.00007;
+    public static double kStatic = 0.00006;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
