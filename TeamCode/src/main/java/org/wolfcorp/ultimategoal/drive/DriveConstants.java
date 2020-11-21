7c49097ee944e4c61d28f6dca2c9d8f83b54358f
BuildTools <unconfigured@null.spigotmc.org>
2020-11-21 10:59:21 -0800

    10/31 Joshua. Localization Testing and more.

--- TeamCode/src/main/java/org/wolfcorp/ultimategoal/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/wolfcorp/ultimategoal/drive/DriveConstants.java
@@ -53,7 +53,7 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.01233;
+    public static double kV = 0.0112;
     public static double kA = 0.00001;
     public static double kStatic = 0.08112;
 
