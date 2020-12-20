b72d79085bde51f5a19393d4e8e76bc0c5597e96
Zhenkai Weng <theoaktree1040@gmail.com>
2020-12-20 12:18:59 -0800

    Remove getLocalizer call

--- TeamCode/src/main/java/org/wolfcorp/ultimategoal/drive/test/LocalizationTest.java
+++ TeamCode/src/main/java/org/wolfcorp/ultimategoal/drive/test/LocalizationTest.java
@@ -42,7 +42,6 @@ public class LocalizationTest extends LinearOpMode {
             telemetry.addData("x", poseEstimate.getX());
             telemetry.addData("y", poseEstimate.getY());
             telemetry.addData("heading", poseEstimate.getHeading());
-            telemetry.addData("Small Encoder", ((ThreeWheelTrackingLocalizer)drive.getLocalizer()).getWheelPositions());
             telemetry.update();
         }
     }
