459c26e7d8879bdfadf8c05ebe54393235cc588c
Zhenkai Weng <theoaktree1040@gmail.com>
2021-03-31 20:27:19 -0700

    Add DemoMode

---
+++ TeamCode/src/main/java/org/wolfcorp/ultimategoal/opmode/DemoMode.java
@@ -0,0 +1,70 @@
+package org.wolfcorp.ultimategoal.opmode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.wolfcorp.ultimategoal.robot.Drivetrain;
+import org.wolfcorp.ultimategoal.robot.Scorer;
+
+@Config
+@TeleOp(name="Testing TeleOp", group = "drive")
+public class DemoMode extends LinearOpMode {
+    Scorer scorer;
+
+    boolean ledOn;
+
+    @Override
+    public void runOpMode() {
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        scorer = new Scorer(hardwareMap);
+        ElapsedTime timer = new ElapsedTime();
+
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Message", "Hello Driver");
+        telemetry.update();
+        // Wait for the game to start (driver presses PLAY)
+
+        waitForStart();
+
+        scorer.openRelease();
+
+        timer.reset();
+        while (opModeIsActive()) {
+            // Reversing scoring mechanisms
+            scorer.reverse(gamepad1.b, 200);
+
+            // Intake
+            scorer.toggleIntake(gamepad1.a && !gamepad1.start, 1, 200);
+            scorer.toggleIntakeFlipper(gamepad1.dpad_right || gamepad2.right_bumper);
+
+            // Outtake
+            scorer.toggleStopper(gamepad1.dpad_left, gamepad1.x, 200);
+            scorer.toggleOuttake(gamepad1.y || gamepad2.y, gamepad1.left_stick_button || gamepad2.a, 200);
+
+            // Wobble Goal
+            scorer.wobbleGripper(gamepad1.left_bumper || gamepad2.left_bumper, 200);
+            scorer.wobbleArm(gamepad1.dpad_down || gamepad2.dpad_down, gamepad1.dpad_up || gamepad2.dpad_up);
+            scorer.moveFlipper(gamepad2.b && !gamepad2.start, gamepad2.x);
+
+            if (gamepad1.left_stick_button && gamepad1.right_stick_button) {
+                scorer.LED.setPattern(RevBlinkinLedDriver.BlinkinPattern.BLACK);
+            }
+            else if (gamepad1.left_stick_button) {
+                scorer.LED.setPattern(RevBlinkinLedDriver.BlinkinPattern.CP1_HEARTBEAT_FAST);
+            }
+            else if (gamepad1.right_stick_button) {
+                scorer.LED.setPattern(RevBlinkinLedDriver.BlinkinPattern.CP2_HEARTBEAT_FAST);
+            }
+        }
+    }
+}
